5a0e71c92e32b0a8b8e76b1c472538082d86e537
team467ironman <team467programmer@gmail.com>
2019-10-24 20:38:01 -0400

    first working iteration, proved the rrequest works

--- datalab/src/main/java/org/shrewsburyrobotics/datalab/App.java
+++ datalab/src/main/java/org/shrewsburyrobotics/datalab/App.java
@@ -1,11 +1,15 @@
 package org.shrewsburyrobotics.datalab;
 
 import org.shrewsburyrobotics.datalab.datafetch.*;
+
+import okhttp3.ResponseBody;
+
 import java.io.IOException;
 
 public class App {
     private datafetcher mDataFetcher;
-
+    private ResponseBody responseTest;
+    
     private void runAwards() {
         mDataFetcher = new datafetcher(300, yearIndex.DEEPSPACE, requestTypes.AWARDS, "MABOS");
         mDataFetcher.addParameter("teamNumber", 467);
@@ -17,8 +21,19 @@ public class App {
         }
     }
 
+    private void recieveBody() {
+       responseTest = mDataFetcher.receiveBody();
+    }
+
+
     public static void main( String[] args ) {
         App app = new App();
         app.runAwards();
+        app.recieveBody();
+
+        if(app.responseTest != null) {
+            System.out.println(app.responseTest);
+        }
+
     }
 }
