29bb0b2fcd52405d41fa74a85fff35822c7cf82f
team467ironman <team467programmer@gmail.com>
2019-11-25 20:57:06 -0500

    attempt at getting profiles untested

--- datalab/src/main/java/org/shrewsburyrobotics/datalab/teamProfile/ProfileBuilder.java
+++ datalab/src/main/java/org/shrewsburyrobotics/datalab/teamProfile/ProfileBuilder.java
@@ -11,48 +11,45 @@ public class ProfileBuilder {
     private int mTeamNumber;
     private yearIndex mSeason;
     private String mEventString;
-    public ArrayList<String> mEventArray = new ArrayList<String>(0);
-    public ArrayList<String> mMatchArray = new ArrayList<String>(0);
-
+    private String mDistrictCode, mDistrictCodeBody;
     private datafetcher mEventDatafetcher;
 
     public ProfileBuilder(int teamNumber, yearIndex season) {
 
         
         //assign team number
-        mTeamNumber = teamNumber;
-        mSeason = season;
+        this.mTeamNumber = teamNumber;
+        this.mSeason = season;
 
-        //find all events
-        try {
-            mEventDatafetcher = new datafetcher()
-                .dataFetch(300, season, requestTypes.EVENTS)
-                .addParameter("districtCode", Double.toString(teamNumber));
-            mEventDatafetcher.sendGet();
-            mEventString = mEventDatafetcher.receiveBody();
-            frcJsonParser mEventfWriter = new frcJsonParser(mEventString, requestTypes.EVENTS);
-            for(int i = 0; i < mEventfWriter.docs.length(); i++) {
-                mEventArray.add(mEventfWriter.docs.getJSONObject(i).toString());
-            }
-        }  catch(IOException e) {
-            e.printStackTrace();
-        }
+        //find all events moved to event list table
+        // try {
+        //     mEventDatafetcher = new datafetcher()
+        //         .dataFetch(300, season, requestTypes.EVENTS)
+        //         .addParameter("districtCode", Double.toString(teamNumber));
+        //     mEventDatafetcher.sendGet();
+        //     mEventString = mEventDatafetcher.receiveBody();
+        //     frcJsonParser mEventfWriter = new frcJsonParser(mEventString, requestTypes.EVENTS);
+        //     for(int i = 0; i < mEventfWriter.docs.length(); i++) {
+        //         mEventArray.add(mEventfWriter.docs.getJSONObject(i).toString());
+        //     }
+        // }  catch(IOException e) {
+        //     e.printStackTrace();
+        // }
 
     }
 
-    public ProfileBuilder addMatches() {
-        datafetcher mMatchDatafetcher = new datafetcher();
-        for(int i = 0; i <= mEventArray.size(); i++) {
+    public ProfileBuilder addDistrictCode() {
+        datafetcher mDatafetcher = new datafetcher();
             try {            
-                mMatchDatafetcher.dataFetch(300, mSeason, requestTypes.MATCHES, mEventArray.get(i))
+                mDatafetcher.dataFetch(300, mSeason, requestTypes.EVENTS)
                 .addParameter("teamNumber", Double.toString(mTeamNumber))
                 .sendGet(); 
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            mMatchArray.add(mMatchDatafetcher.receiveBody());
-        }
+            this.mDistrictCodeBody = mDatafetcher.receiveBody();
+            frcJsonParser mDistrictWriter = new frcJsonParser(this.mDistrictCodeBody, requestTypes.EVENTS);
+            this.mDistrictCode = mDistrictWriter.docs.getJSONObject(4).toString();
 
         return this;
     }
